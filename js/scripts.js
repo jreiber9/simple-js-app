52846a175e76cdc07e0b99e0bee924400886cff4
jreiber9 <jreiber9@outlook.com>
2023-04-10 16:39:23 -0400

    update to JS for 2nd exercise
    Added pokedex array as well as pokemon objects; replaced old contents of file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-alert('Hello world');
+//Blank Pokedex Array
 
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [];
+
+//Pokedex Array Objects (inluding name, height, and type)
+
+pokemonList[0] ={
+    name: "Bulbasaur",
+    height: .7,
+    types: ['grass', 'poison']
+};
+
+pokemonList[1] ={
+    name: "Charmander",
+    height: .6,
+    types: ['fire']
+};
+
+pokemonList[2] ={
+    name: "Squirtle",
+    height: .5,
+    types: ['water']
+};
