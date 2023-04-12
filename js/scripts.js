67c2d90d23efa9322e9891f70eecd46a18a45e08
jreiber9 <jreiber9@outlook.com>
2023-04-12 11:21:10 -0400

    remformatted array rec by mentor
    reformatted the array to list to put all objects within one array grouping

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,8 @@
-//Blank Pokedex Array
-
-let pokemonList = [];
-
 //Pokedex Array Objects (inluding name, height, and type)
 
-pokemonList[0] ={
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ['grass', 'poison']
-};
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Charmander', height: 0.6, types: ['fire']},
+    { name: 'Squirtle', height: 0.5, types: ['water']},
+];
 
-pokemonList[1] ={
-    name: "Charmander",
-    height: 0.6,
-    types: ['fire']
-};
-
-pokemonList[2] ={
-    name: "Squirtle",
-    height: 0.5,
-    types: ['water']
-};
