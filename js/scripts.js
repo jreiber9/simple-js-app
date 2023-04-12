63d6b917ce33e557ed022c79f4eae4489bebfd0a
jreiber9 <jreiber9@outlook.com>
2023-04-12 12:51:04 -0400

    Added loop
    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,15 @@ let pokemonList = [
     { name: 'Squirtle', height: 0.5, types: ['water']},
 ];
 
+//For loop that iterates pokemon names and adds sauce if height exceeds or is lower than .6
+
+
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > .6 ){
+        document.write(pokemonList[i].name + ' (height:' +pokemonList[i].height + ')' + ' - Wow, that\'s big!'+"<br>");
+    }else if (pokemonList[i].height <.6 ){
+        document.write(pokemonList[i].name + ' (height:' +pokemonList[i].height + ')' + ' - Wow that\'s tiny!'+"<br>");
+    }else {
+        document.write(pokemonList[i].name + ' (height:' +pokemonList[i].height + ')'+"<br>");
+    }
+}
